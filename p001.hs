4273a0f76d7547134745e46bbc03ac5845d77512
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2019-03-27 14:53:16 +0900

    prob 1 done

---
+++ p001.hs
@@ -0,0 +1,21 @@
+-- 1
+-- If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+-- The sum of these multiples is 23.
+-- Find the sum of all the multiples of 3 or 5 below 1000.
+
+sumOfThreeOrFiveMultTo :: Integer -> Integer
+sumOfThreeOrFiveMultTo n
+  | n <= 0    = 0
+  | otherwise = sum $ filter fiveOrThreeMult [1..n-1]
+
+fiveOrThreeMult :: Integer -> Bool
+fiveOrThreeMult n
+  | n <= 0         = False
+  | n `mod` 3 == 0 = True
+  | n `mod` 5 == 0 = True
+  | otherwise      = False
+
+main = do
+  print $ "the answer is "
+  print $ sumOfThreeOrFiveMultTo 10
+  print $ sumOfThreeOrFiveMultTo 1000
